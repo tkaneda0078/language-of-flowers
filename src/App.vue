0a1af62c510d6e67e34453c7c07098664b1a35bf
金田尚大 <1111510078r21@gmail.com>
2019-06-28 18:38:11 +0900

    headerコンポーネント

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <Header/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header id="header">
+    <h1>header area</h1>
+  </header>
+</template>
